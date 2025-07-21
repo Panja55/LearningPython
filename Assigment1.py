453f83e8577527f5233893a7552668d664ba1169
Panja55 <72296531+Panja55@users.noreply.github.com>
2025-07-21 07:40:42 +0530

    uploading Assignment1

---
+++ Assigment1.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the first number: "))
+print("Addition: ", num1+num2)
+print("Subtraction: ", num1-num2)
+print("Multiplication: ", num1*num2)
+if num2==0:
+    print("Indivisible as the second number is 0.")
+else:
+    print("Division: ", num1 / num2)
